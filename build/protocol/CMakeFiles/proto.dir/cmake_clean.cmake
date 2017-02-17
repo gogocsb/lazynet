78dab793e63bdb0018469e17fe816e6126312dbf
gogocsb <646103149@qq.com>
2017-02-17 16:32:24 +0800

    create repo

---
+++ build/protocol/CMakeFiles/proto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "DS_STORE_PROTOCOL.pb.cc"
+  "DS_STORE_PROTOCOL.pb.h"
+  "CMakeFiles/proto.dir/DS_STORE_PROTOCOL.pb.cc.o"
+  "../../lib/libproto.pdb"
+  "../../lib/libproto.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/handy.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/lazynet/src/handy/codec.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/codec.cc.o"
+  "/root/lazynet/src/handy/conf.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/conf.cc.o"
+  "/root/lazynet/src/handy/conn.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/conn.cc.o"
+  "/root/lazynet/src/handy/daemon.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/daemon.cc.o"
+  "/root/lazynet/src/handy/event_base.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/event_base.cc.o"
+  "/root/lazynet/src/handy/file.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/file.cc.o"
+  "/root/lazynet/src/handy/http.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/http.cc.o"
+  "/root/lazynet/src/handy/logging.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/logging.cc.o"
+  "/root/lazynet/src/handy/net.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/net.cc.o"
+  "/root/lazynet/src/handy/poller.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/poller.cc.o"
+  "/root/lazynet/src/handy/port_posix.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/port_posix.cc.o"
+  "/root/lazynet/src/handy/stat-svr.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/stat-svr.cc.o"
+  "/root/lazynet/src/handy/threads.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/threads.cc.o"
+  "/root/lazynet/src/handy/udp.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/udp.cc.o"
+  "/root/lazynet/src/handy/util.cc" "/root/lazynet/build/src/CMakeFiles/handy.dir/handy/util.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OS_LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
